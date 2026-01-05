8641324166785912fa23bb0ed5be40aef96da8b7
Wagner Brüggemann <wagner.bruggemann@gmail.com>
2026-01-05 15:16:22 -0300

    [base] feat: Project Start

---
+++ template/src/test/java/com/namgrengaw/template/TemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.namgrengaw.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
